2d8e12ebb12636e3a8e486b352ec26e6b2e32a70
cyuniarti2 <cyuniarti2@gmail.com>
2025-07-08 18:29:02 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# bokepterbaruindo
-bokepterbaruindo
+
+[bokepterbaruindo](https://bokepterbaruindo.pages.dev)
---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const url = 'https://bokepterbaruindo.pages.dev';
+export const nama = 'Bokep Terbaru Indo';
+export const desk = 'Video Bokep Indo Terbaru Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante';
+export const terbit = '2025-03-09T00:01:03Z';
+
+export const google = 'Ufje-Am0tBRo2bOwf5boMoR3mdtQCpROwnYPmjek9vE';
+export const yandex = 'c0a52edef7821480';
+
+export const index = 'f257e50b-227d-4939-8a2a-27e905f69fea';
